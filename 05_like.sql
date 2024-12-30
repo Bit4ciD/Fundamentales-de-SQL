649c45ae0457d2ed8b921b59fd3bd4c62aebd785
Bit4ciD <bitacid@gmail.com>
2024-12-30 12:20:43 +0100

    Fundamentos de SQL

--- 05_like.sql
+++ 05_like.sql
@@ -1,1 +1,8 @@
---
+-- Muestra todos los usuarios que tienen un email acabado en @gmail.com
+SELECT * FROM users WHERE email LIKE '%gmail.com';
+
+-- Muestra todos los registros que tienen un email que empieza con sara 
+SELECT * FROM users WHERE email LIKE 'sara%';
+
+-- Muestra todos los registros que tienen un email que contienen una arroba 
+SELECT * FROM users WHERE email LIKE '%@%";
