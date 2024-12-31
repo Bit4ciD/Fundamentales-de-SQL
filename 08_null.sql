db09d1d66887472d4c9a0f96b6955dfda40e8ebd
Bit4ciD <bitacid@gmail.com>
2024-12-31 01:01:43 +0100

    Version 10

--- 08_null.sql
+++ 08_null.sql
@@ -1,8 +1,8 @@
---
+-- Muestra los registros de los usuarios que tienen el email null
 SELECT * FROM users WHERE email ISNULL; 
 
---
+-- Muestra los registros de los usuarios que no tienen el email null
 SELECT * FROM users WHERE email IS NOT NULL; 
 
---
+-- Muestra los registros de los usuarios que no tienen el email null y tienen una edad de 15
 SELECT * FROM users WHERE email IS NOT NULL AND age = 15; 
