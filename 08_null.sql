81a685c76f7539b67ef88f899d865b96ae1489d6
Bit4ciD <bitacid@gmail.com>
2025-01-03 19:43:25 +0100

    13

--- 08_null.sql
+++ 08_null.sql
@@ -6,3 +6,6 @@ SELECT * FROM users WHERE email IS NOT NULL;
 
 -- Muestra los registros de los usuarios que no tienen el email null y tienen una edad de 15
 SELECT * FROM users WHERE email IS NOT NULL AND age = 15; 
+
+--
+SELECT name, surname, IFNULL(age, 0) AS age FROM users;
